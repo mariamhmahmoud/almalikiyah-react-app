c8f26e0cb67bb9982d7bee4f30dfc0487ea278c9
Mariam Mahmoud <mariamhm@umich.edu>
2023-07-14 12:16:33 -0400

    Hardcoded url and removed proxy

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -19,7 +19,7 @@ export default function Admin() {
 
 
     const refreshToken = async () => {
-        fetch('/token')
+        fetch('https://expressjs-server-production-754b.up.railway.app/token')
             .then(response => {
                 if (response.ok) {
                     return response.json();
@@ -44,7 +44,7 @@ export default function Admin() {
     axiosJWT.interceptors.request.use(async (config) => {
         const currentDate = new Date();
         if (expire * 1000 < currentDate.getTime()) {
-            const response = await axios.get('/token');
+            const response = await axios.get('https://expressjs-server-production-754b.up.railway.app/token');
             config.headers.Authorization = `Bearer ${response.data.accessToken}`;
             setToken(response.data.accessToken);
             const decoded = jwt_decode(response.data.accessToken);
